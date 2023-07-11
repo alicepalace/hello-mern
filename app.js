a8b256426a15f79f5e7731d0e30432a8d2698813
Alice Graham <alice.graham2@mail.mcgill.ca>
2023-07-11 18:03:33 -0400

    added ugly css

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index', {title: "Welcome 2 birth"});
 });
+
+app.get('/exercises', (req, res) => {
+    res.render('exercises', {title: "Workout Regimen"});
+})
 
